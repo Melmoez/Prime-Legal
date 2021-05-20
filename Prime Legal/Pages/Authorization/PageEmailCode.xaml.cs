071219664554628a0b753fd0f57554eab56ab113
Melmoez <68968481+Melmoez@users.noreply.github.com>
2021-05-20 15:46:04 +0300

    Добавил модель

---
+++ Prime Legal/Pages/Authorization/PageEmailCode.xaml.cs
@@ -0,0 +1,35 @@
+﻿using Prime_Legal.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Prime_Legal.Pages.Authorization
+{
+    /// <summary>
+    /// Логика взаимодействия для PageEmailCode.xaml
+    /// </summary>
+    public partial class PageEmailCode : Page
+    {
+        public PageEmailCode()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            ActionWindowClass.MainFrame.NavigationService.RemoveBackEntry();
+            ActionWindowClass.MainFrame.Navigate(new PageAuthorization());
+        }
+    }
+}
