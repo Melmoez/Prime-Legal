45108ecd8a7bf3172b3dca58b63e0c894e92b446
Melmoez <68968481+Melmoez@users.noreply.github.com>
2021-05-22 13:41:06 +0300

    Сделал анимацию

--- Prime Legal/Windows/WinAdmin.xaml.cs
+++ Prime Legal/Windows/WinAdmin.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
@@ -25,18 +26,41 @@ namespace Prime_Legal.Windows
             InitializeComponent();
             BtnClose.Click += (sender, e) => { ActionWindowClass.CloseStateButton(); };
             BtnMinimize.Click += (sender, e) => { ActionWindowClass.MinimizedStateButton(this); };
+            DoubleAnimation buttonAnimation = new DoubleAnimation();
         }
 
         private void BtnOpenMenu_Click(object sender, RoutedEventArgs e)
         {
             BtnOpenMenu.Visibility = Visibility.Collapsed;
             BtnCloseMenu.Visibility = Visibility.Visible;
+
+            DoubleAnimation GridMenuOpen = new DoubleAnimation();
+            DoubleAnimation LineMenuOpen = new DoubleAnimation();
+            GridMenuOpen.From = GridHamburger.ActualWidth;
+            LineMenuOpen.From = RectangleMenu.ActualWidth;
+            GridMenuOpen.To = 376;
+            LineMenuOpen.To = 356;
+            LineMenuOpen.Duration = TimeSpan.FromSeconds(0.1);
+            GridMenuOpen.Duration = TimeSpan.FromSeconds(0.1);
+            RectangleMenu.BeginAnimation(Rectangle.WidthProperty, LineMenuOpen);
+            GridHamburger.BeginAnimation(Grid.WidthProperty, GridMenuOpen);
         }
 
         private void BtnCloseMenu_Click(object sender, RoutedEventArgs e)
         {
             BtnOpenMenu.Visibility = Visibility.Visible;
             BtnCloseMenu.Visibility = Visibility.Collapsed;
+
+            DoubleAnimation GridMenuClose = new DoubleAnimation();
+            DoubleAnimation LineMenuClose = new DoubleAnimation();
+            GridMenuClose.From = GridHamburger.ActualWidth;
+            LineMenuClose.From = RectangleMenu.ActualWidth;
+            GridMenuClose.To = 70;
+            LineMenuClose.To = 47;
+            LineMenuClose.Duration = TimeSpan.FromSeconds(0.1);
+            GridMenuClose.Duration = TimeSpan.FromSeconds(0.1);
+            RectangleMenu.BeginAnimation(Rectangle.WidthProperty, LineMenuClose);
+            GridHamburger.BeginAnimation(Grid.WidthProperty, GridMenuClose);
         }
     }
 }
